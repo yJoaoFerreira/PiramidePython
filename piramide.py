4bf95fae49afdb21c60100d276db4178ab232b99
João Ferreira <yFerreiraJoao@gmail.com>
2024-08-25 11:36:40 -0300

    Update piramide.py

--- piramide.py
+++ piramide.py
@@ -1,4 +1,6 @@
 print(f"Coloque apenas números de 1 único digito, pois se não irá deformar a pirâmide.")
+print("")
+
 n = 5
 for i in range (n):
     print((n-i-1)*" ", (str(n-i) + " ") * i + str(n-i))
